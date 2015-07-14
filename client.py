f214e97514ee8fa470dab598d6055f0c353254bd
yanchao727 <yvogue@y01.ydc.yvogue.com>
2015-07-14 10:52:13 +0800

    first concrete version

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ from time import gmtime, strftime
 import requests
 import json
 import hashlib
+import urllib
 
 class Shangpin_client(object):
     def __init__(self):
@@ -27,24 +28,36 @@ class Shangpin_client(object):
         print(strftime("%Y-%M-%d %H:%M"))
 
     def init_data(self):
-        self.timestamp = strftime("%Y-%M-%d %H:%M")
-        self.data = {'app_key': self.app_key, 'timestamp': self.timestamp}
-        md5_sign = "app_key=" + self.app_key + "&timestamp=" + self.timestamp + "_" + self.app_secret
+        self.timestamp = strftime("%Y-%m-%d %H:%M")
+        self.data = {'app_key': self.app_key.encode("utf-8"), 'timestamp': self.timestamp.encode("utf-8")}
+
+    def set_sign(self):
+        md5_sign = "app_key=" + urllib.quote_plus(self.app_key.encode("utf-8")) + "&request=" + urllib.quote_plus(self.data['request'])+ "&timestamp=" + urllib.quote_plus(self.timestamp.encode('utf-8')) + "_" + self.app_secret.encode('utf-8')
+        #md5_sign = "app_key=" + urllib.quote_plus(self.app_key.encode("utf-8")) + "&request=" + self.data['request'] + "&timestamp=" + urllib.quote_plus(self.timestamp.encode('utf-8')) + "_" + self.app_secret.encode('utf-8')
+        print("md5_sign")
+        print(md5_sign)
+        #md5_sign = "app_key=" + self.app_key + "&request=" + self.data['request']+ "&timestamp=" + self.timestamp + "_" + self.app_secret
+        #print(md5_sign)
         m = hashlib.md5()
         m.update(md5_sign)
         self.sign = m.hexdigest()
-        self.data['sign'] = self.sign
+        self.data['sign'] = self.sign.upper()
 
     def set_data(self):
         pass
 
     def req_post(self):
-        print("params:")
-        print(self.params)
+        request_val = {"PageIndex":"1","PageSize":"100","endTime":"","startTime":""}
+        print(json.dumps(request_val))
+        self.data['request'] = json.dumps(request_val, sort_keys=True, ensure_ascii=False).encode('utf8')
+        #self.data['request'] = urllib.quote_plus(json.dumps(request_val, sort_keys=True))
+        self.set_sign()
         print("data:")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         print(self.data)
-        #response = requests.post(self.url+self.path, params = self.params, data=json.dumps(self.data), headers=self.headers)
-        response = requests.post(self.url+self.path, params = self.params, data=self.data, headers=self.headers)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        #response = requests.post(self.url+self.path, data=urllib.urlencode(self.data), headers=self.headers)
+        response = requests.request('POST', self.url+self.path, data=self.data, headers=self.headers)
         print(response.content)
 
     def set_path(self, path_value):
--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from client import *
 shangpin_client = Shangpin_client()
-shangpin_client.set_path('/commodity/findinfo')
+shangpin_client.set_path('/commodity/findinfobypage')
 shangpin_client.req_post()
